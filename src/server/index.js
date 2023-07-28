fb3fb4b9cb12d8075a139e6fd0eea9355255520c
tolmdyn <tolmdyn@gmail.com>
2023-07-28 15:18:21 +0100

    server impl

--- src/server/index.js
+++
@@ -1,3 +0,0 @@
-/**
- * @fileoverview Server entry point
- */
---
+++ src/server/server.js
@@ -0,0 +1,141 @@
+/**
+ * @fileoverview Server entry point
+ */
+
+const WebSocket = require('ws');
+const debug = require('debug')('server');
+const path = require('path');
+
+const { Database } = require('../database/database');
+const { documentSchema } = require('../models/validation');
+const { Types, isValidItemType } = require('../models/types');
+const { getHash } = require('../utils/utils');
+
+const dbPath = path.join(__dirname, './../../data/database.db');
+const database = new Database(dbPath);
+
+const RequestTypes = {
+  Get: 'get',
+  Put: 'put',
+  Ping: 'ping',
+  Message: 'message',
+};
+
+const ResponseTypes = {
+  Success: 'success',
+  Error: 'error',
+};
+
+class Response {
+  constructor(type, message) {
+    this.type = type;
+    this.message = message;
+  }
+}
+
+function handleRequest(message) {
+  // const requestString = message.toString();
+  const request = JSON.parse(message); // concat these two lines
+
+  debug('Request:', request);
+
+  const { requestType, requestData } = request;
+
+  if (requestType === RequestTypes.Get) {
+    return handleGet(requestData);
+  }
+
+  if (requestType === RequestTypes.Put) {
+    return handlePut(requestData);
+  }
+
+  if (requestType === RequestTypes.Ping) {
+    return handlePing();
+  }
+
+  if (requestType === RequestTypes.Message) {
+    return handleMessage(requestData);
+  }
+
+  return 'Invalid request type';
+}
+
+function handleGet(request) {
+  const { key, type } = request;
+
+  if (!key || !type) {
+    return 'Invalid request, missing parameters';
+  }
+
+  if (!isValidItemType(type)) {
+    return 'Invalid item type';
+  }
+
+  try {
+    debug(`Getting item from database\nKey: ${key}\nType: ${type}`);
+    const item = database.get(key, type);
+
+    // Do some things here?
+    if (item) {
+      return new Response(ResponseTypes.Success, item);
+    }
+    return new Response(ResponseTypes.Success, 'Item not found in database.');
+  } catch (error) {
+    // Possibly massage the error message to make it more readable for users
+    return new Response(ResponseTypes.Error, error.message);
+  }
+}
+
+function handlePut(request) {
+  const { type, data } = request;
+
+  if (!type || !data) {
+    return new Response(ResponseTypes.Error, 'Invalid request, missing parameters.');
+  }
+
+  if (!isValidItemType(type)) {
+    return new Response(ResponseTypes.Error, 'Invalid item type.');
+  }
+
+  const key = getHash(data);
+
+  try {
+    debug(`Putting item into database\nKey: ${key}\nType: ${type}\nData: ${data}`);
+    const result = database.put(key, type, data);
+    // Do some things here?
+    // check if result is valid?
+    if (!result) {
+      return new Response(ResponseTypes.Error, 'Error putting item into database.');
+    }
+    return new Response(ResponseTypes.Success, `Item ${result.key} inserted into database.`);
+  } catch (error) {
+    // Possibly massage the error message to make it more readable for users
+    return new Response(ResponseTypes.Error, error.message);
+  }
+}
+
+function handlePing() {
+  debug('Sending pong.');
+  return new Response(ResponseTypes.Success, 'Pong.');
+}
+
+function handleMessage(request) {
+  debug('Handling message.');
+  const { message } = request;
+  console.log('Message from client:', message);
+  return new Response(ResponseTypes.Success, 'Message received.');
+}
+
+const server = new WebSocket.Server({ port: 8080 });
+
+server.on('connection', (socket, request) => {
+  debug(`Connection: ${request.socket.remoteAddress}:${request.socket.remotePort}`);
+  socket.on('message', (message) => {
+    const response = handleRequest(message.toString());
+    socket.send(JSON.stringify(response));
+  });
+});
+
+debug(`Server running at ${server.address().address}:${server.address().port}`);
+
+module.exports = { server };
