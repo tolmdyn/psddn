90d88bb43cce808102b33c6486d16f204af7a1ac
tolmdyn <tolmdyn@gmail.com>
2023-08-05 14:48:57 +0100

    client get, put, pub functions

--- src/client/client.js
+++ src/client/client.js
@@ -33,9 +33,9 @@ const { authenticateUser, createNewUser } = require('../auth/auth');
 const { RequestTypes, Request } = require('../models/request');
 const { ResponseTypes, Response } = require('../models/response');
 const { isValidItemType } = require('../models/types');
-const { isKey, isValidKey } = require('../utils/utils');
-const { getProviders } = require('../network/routing');
-const { de } = require('@faker-js/faker');
+const { isValidKeyFormat, generateKey } = require('../utils/utils');
+const { getProviders } = require('../network/providers');
+const { validateItem } = require('../models/validation');
 
 let userSession = null;
 
@@ -62,15 +62,14 @@ async function getItem(key, type) {
     return new Response(ResponseTypes.Error, 'Invalid item type.');
   }
 
-  if (!isKey(key)) {
+  if (!isValidKeyFormat(key)) {
     return new Response(ResponseTypes.Error, 'Invalid key format.');
   }
 
   // check local db // should this be a seperate function?
   try {
     debug(`Getting item from local database\nKey: ${key}\nType: ${type}`);
-    // const item = database.get(key, type);
-    let item;
+    const item = database.get(key, type);
 
     if (item) {
       return new Response(ResponseTypes.Success, item);
@@ -95,101 +94,115 @@ async function getItem(key, type) {
 async function getItemFromProviders(key, type) {
   const providers = getProviders(key, type);
 
-  if (!providers) {
+  if (!providers || providers.length === 0) {
     return new Response(ResponseTypes.Error, 'No providers found for item.');
   }
 
   const request = new Request(RequestTypes.Get, { key, type });
 
-  async function tryConnectToProvider(provider) {
-    try {
-      const ws = new WebSocket(`ws://${provider.ip}:${provider.port}`);
-      await new Promise((resolve) => ws.on('open', resolve)); // wait for connection to open before continuing
-      ws.send(JSON.stringify(request));
-
-      let responseRecieved = false;
-
-      const responsePromise = new Promise((resolve, reject) => {
-        ws.on('message', (message) => {
-          responseRecieved = true;
-
-          const response = JSON.parse(message);
-          debug('Response:', response);
-          
-          if (response.type === ResponseTypes.Success) {
-            ws.close();
-            resolve(response);
-          } else if (response.type === ResponseTypes.Error) {
-            ws.close();
-            resolve(null);
-          }
-        });
-
-        ws.on('error', (err) => {
-          debug('Websocket error:', err);
-          reject(err);
-        });
-      });
-
-      const timeoutPromise = new Promise((_, reject) => {
-        const timeoutId = setTimeout(() => {
-          if (!responseRecieved) {
-            ws.close();
-            debug('Connection or response timed out.');
-            reject(new Error('Connection or response timed out.'));
-          }
-        }, 10000); // 10 seconds timeout, adjust as needed
-
-        // if response is recieved before timeout, cancel the timeout
-        responsePromise.finally(() => clearTimeout(timeoutId));
-      });
-
-      // Use Promise.race to resolve with the first resolved promise
-      return Promise.race([responsePromise, timeoutPromise]);
-    } catch (err) {
-      debug('Error connecting to provider:', err);
-      return null;
-    }
-  }
-
-  // a for loop is synchronous, so we should use something else with await
-  for (let i = 0; i < providers.length; i += 1) {
-    debug('Trying provider:', providers[i]);
-    const response = await tryConnectToProvider(providers[i]);
-    if (response !== null) {
-      return new Response(ResponseTypes.Success, response.message);
-    }
-  }
-
   // map the array of providers to an array of promises
-  // const promises = providers.map((provider) => tryConnectToProvider(provider));
+  const promises = providers.map((provider) => sendRequestToProvider(request, provider));
 
-  // // wait for all promises to resolve
-  // const results = await Promise.all(promises);
-  // console.log('Results:', JSON.stringify(results));
-  // // find successful responses, is this redundant?
-  // const validResults = results.filter((result) => result !== null);
-  // console.log('Valid results:', JSON.stringify(validResults));
+  // wait for all promises to resolve
+  const results = await Promise.all(promises);
 
-  // const successfulResult = validResults.find((result) => result.type === ResponseTypes.Success);
-  // if (successfulResult) {
-  //   return successfulResult;
-  // }
+  // find successful responses
+  const successfulResult = results.filter((result) => result !== null)
+    .find((result) => result.type === ResponseTypes.Success);
+
+  if (successfulResult) {
+    return successfulResult;
+  }
 
   // if no item recieved, return error
   return new Response(ResponseTypes.Error, 'No provider has the item.');
 }
 
-function putItem(key, value, type) {
-  // add to local db only
-  // update feed?
+/**
+ * @description Adds an item to the local database, Key and type are generated from the item.
+ * @param {Object} item The item to be added to the database.
+ */
+function putItem(item) {
+  // validate item
+  if (!item) {
+    return new Response(ResponseTypes.Error, 'Invalid request, missing item.');
+  }
 
+  if (!isValidItemType(item.type)) {
+    return new Response(ResponseTypes.Error, 'Invalid item type.');
+  }
+
+  if (!validateItem(item)) {
+    return new Response(ResponseTypes.Error, `Provided item is not a valid ${item.type}.`);
+  }
+
+  // add to local db only
+  const key = generateKey(item);
+  const { type } = item;
+
+  try {
+    debug(`Putting item into local database\nKey: ${key}\nType: ${type}\nData: ${JSON.stringify(item)}`);
+    const result = database.put(key, type, item);
+    if (!result) {
+      return new Response(ResponseTypes.Error, 'Error putting item into database.');
+    }
+    return new Response(ResponseTypes.Success, `Item ${result.key} inserted into database.`);
+  } catch (error) {
+    return new Response(ResponseTypes.Error, error.message);
+  }
 }
 
-function pubItem(key, value, type) {
-  // add to local db
+function pubItem(item) {
+  // validate item
+  if (!item) {
+    return new Response(ResponseTypes.Error, 'Invalid request, missing item.');
+  }
+
+  if (!isValidItemType(item.type)) {
+    return new Response(ResponseTypes.Error, 'Invalid item type.');
+  }
+
+  if (!validateItem(item)) {
+    return new Response(ResponseTypes.Error, `Provided item is not a valid ${item.type}.`);
+  }
+
+  // add to local db only
+  const key = generateKey(item);
+  const { type } = item;
+
+  // if not in local db then add to local db
+  try {
+    debug(`Putting item into local database\nKey: ${key}\nType: ${type}\nData: ${JSON.stringify(item)}`);
+    const result = database.put(key, type, item);
+    if (!result) {
+      return new Response(ResponseTypes.Error, 'Unknown error putting item into database.');
+    }
+    // return new Response(ResponseTypes.Success, `Item ${result.key} inserted into database.`);
+    debug('Item added to local database.');
+  } catch (error) {
+    if (error.message !== 'Key already exists in database.') {
+      debug('Error putting item into local database:', error);
+      return new Response(ResponseTypes.Error, error.message);
+    }
+    debug('Item not added to local database, already exists. Continuing...');
+  }
+
   // update feed
+  updateUserFeed(item);
+
   // send to peers / dht
+  try {
+    debug(`Sending ${key} to providers...`);
+    const result = sendItemToProviders(key, type, item);
+    if (!result) {
+      return new Response(ResponseTypes.Error, 'Error sending item to providers.');
+    }
+    return result;
+    // return new Response(ResponseTypes.Success, 'Item sent to providers.');
+  } catch (error) {
+    debug('Error sending item to providers:', error);
+    return new Response(ResponseTypes.Error, error.message);
+  }
 }
 
 function updateFeeds() {
@@ -202,40 +215,152 @@ function updateFeeds() {
   // update local db
 }
 
-function updateUserFeed() {
+function updateUserFeed(item) {
   // get user feed
   // append new items
   // update local db
 }
 
-// console.log(getItem('20292bf632e04f4c', 'document'));
+/** -------------------------- internal use functions ---------------------------------- */
+
+/**
+ * @description Generic function to sends a "request" (e.g. GET, PUT, etc) to a
+ * provider.
+ * @param {Request} request The request to send to the providers. Containing the key,
+ * type and/or data of the item.
+ */
+async function sendRequestToProvider(request, provider) {
+  try {
+    const ws = new WebSocket(`ws://${provider.ip}:${provider.port}`);
+
+    return new Promise((resolve, reject) => {
+      ws.on('error', (err) => {
+        debug('Websocket error:', err);
+        // reject(err);
+        // Should it resolve with the error Response instead?
+        resolve(null);
+      });
+
+      // wait for connection to open before continuing
+      // await new Promise((resolve) => { ws.on('open', resolve); });
+
+      ws.on('open', () => {
+        ws.send(JSON.stringify(request));
+
+        let responseRecieved = false;
+
+        const responsePromise = new Promise((resResolve) => {
+          ws.on('message', (message) => {
+            responseRecieved = true;
+
+            const response = JSON.parse(message);
+            debug('Response:', response);
+
+            if (response.type === ResponseTypes.Success) {
+              ws.close();
+              resResolve(response);
+            } else if (response.type === ResponseTypes.Error) {
+              ws.close();
+
+              // Should it resolve with the error Response instead?
+              resResolve(null);
+            }
+          });
+        });
+
+        const timeoutPromise = new Promise((_, timeoutReject) => {
+          const timeoutId = setTimeout(() => {
+            if (!responseRecieved) {
+              ws.close();
+              debug('Connection or response timed out.');
+              timeoutReject(new Error('Connection or response timed out.'));
+            }
+          }, 10000); // 10 seconds timeout, adjust as needed
+
+          // if response is recieved before timeout, cancel the timeout
+          responsePromise.finally(() => clearTimeout(timeoutId));
+        });
+
+        // Use Promise.race to resolve with the first resolved promise
+        return Promise.race([responsePromise, timeoutPromise])
+          .then((result) => { resolve(result); })
+          .catch((err) => { reject(err); });
+      });
+    });
+  } catch (err) {
+    debug('Error connecting to provider:', err);
+    return null;
+  }
+}
+
+async function sendItemToProviders(key, type, data) {
+  const providers = getProviders(key, type);
+
+  // if no providers found, return error
+  if (!providers || providers.length === 0) {
+    return new Response(ResponseTypes.Error, 'No providers found for item.');
+  }
+
+  const request = new Request(RequestTypes.Put, { key, type, data });
+
+  // map the array of providers to an array of promises
+  const promises = providers.map((provider) => sendRequestToProvider(request, provider));
+
+  // wait for all promises to resolve
+  const results = await Promise.all(promises);
+
+  const successfulResponsesCount = results
+    .filter((result) => result !== null && result.type === ResponseTypes.Success).length;
+
+  debug('Number of successful responses:', successfulResponsesCount);
+
+  if (successfulResponsesCount > 0) {
+    return new Response(ResponseTypes.Success, `Item sent to ${successfulResponsesCount} providers.`);
+  }
+
+  // if successful responses, return error
+  return new Response(ResponseTypes.Error, 'No provider recieved the item.');
+}
+
+/* -------------------------------- Temporary tests -------------------------------- */
 
 async function getTest(key) {
   const res = await getItem(key, 'document');
   console.log('Trying to get item', key, ':\n', res);
 }
 
-getTest('20292bf632e04f4c');
-getTest('0000000000000000');
+async function test() {
+  getTest('20292bf632e04f4c');
+  getTest('0000000000000000');
+}
 
-// function connectToProvider(provider) {
-//   return new Promise((resolve, reject) => {
-//     const ws = new WebSocket(`ws://${provider.ip}:${provider.port}`);
+test();
 
-//     ws.on('open', () => {
-//       ws.send(JSON.stringify(request));
-//     });
+const doc = {
+  type: 'document',
+  id: '1231231231231999',
+  owner: '20292bf632e04f4c',
+  timestamp: '2021-03-25T18:00:00.000Z',
+  title: 'Second Test Document',
+  content: 'This is a test document.',
+  tags: ['test', 'document'],
+};
 
-//     ws.on('message', (message) => {
-//       const response = JSON.parse(message);
-//       if (response.responseType === ResponseTypes.Success) {
-//         ws.close();
-//         resolve(response.item);
-//       }
-//     });
+// The problem with testing is that we need to use two separate databases for pubbing.
+async function pubTest(item) {
+  const res = await pubItem(item);
+  console.log('>> Pubbing item Response:', res);
+}
 
-//     ws.on('error', (err) => {
-//       reject(err);
-//     });
-//   });
-// }
+// pubTest(doc);
+
+/* -------------------------------- ----------------- -------------------------------- */
+
+module.exports = {
+  authenticateUserSession,
+  getItem,
+  putItem,
+  pubItem,
+  updateFeeds,
+  updateUserFeed,
+};
