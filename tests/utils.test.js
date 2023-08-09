2a5558c0e9214169af8036c3c2551d52d83dcb1e
tolmdyn <tolmdyn@gmail.com>
2023-08-09 16:19:22 +0100

    working on tests - not passing

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -4,7 +4,9 @@
 
 const { expect } = require('chai');
 
-const { getHash, isValidKey, generateRandomDocument } = require('../src/utils/utils');
+const {
+  generateKey, isValidKeyForItem, isValidKeyFormat, generateRandomDocument,
+} = require('../src/utils/utils');
 const { documentSchema } = require('../src/models/validation');
 
 describe('Utils Tests', () => {
@@ -45,42 +47,46 @@ describe('Utils Tests', () => {
 
   it('should generate a valid hash', () => {
     const doc = generateRandomDocument();
-    const key = getHash(doc);
-    expect(isValidKey(key, doc)).to.be.true;
+    const key = generateKey(doc);
+    expect(isValidKeyFormat(key)).to.be.true;
+    expect(isValidKeyForItem(key, doc)).to.be.true;
   });
 
   it('should not generate a valid hash', () => {
-    const doc = generateRandomDocument();
-    const key = getHash(doc);
-    expect(isValidKey(key, 'invalid')).to.be.false;
+    const doc1 = generateRandomDocument();
+    const doc2 = generateRandomDocument();
+    const key = generateKey(doc1);
+    expect(isValidKeyForItem(key, doc1)).to.be.true;
+    expect(isValidKeyForItem(key, doc2)).to.be.false;
   });
 
   it('should not accept wrong key', () => {
     const doc = generateRandomDocument();
     const key = '123456789ABCDEF';
-    expect(isValidKey(key, doc)).to.be.false;
+    expect(isValidKeyForItem(key, doc)).to.be.false;
+    expect(isValidKeyFormat(key)).to.be.false;
   });
 
   it('should generate a consistent hash for same data', () => {
     const doc = generateRandomDocument();
-    const key1 = getHash(doc);
-    const key2 = getHash(doc);
+    const key1 = generateKey(doc);
+    const key2 = generateKey(doc);
     expect(key1).to.equal(key2);
   });
 
   it('should generate a different hash for different data', () => {
     const doc1 = generateRandomDocument();
     const doc2 = generateRandomDocument();
-    const key1 = getHash(doc1);
-    const key2 = getHash(doc2);
+    const key1 = generateKey(doc1);
+    const key2 = generateKey(doc2);
     expect(key1).to.not.equal(key2);
   });
 
   it('should generate the same hash for different object with same data', () => {
     const doc1 = generateRandomDocument();
     const doc2 = { ...doc1 };
-    const key1 = getHash(doc1);
-    const key2 = getHash(doc2);
+    const key1 = generateKey(doc1);
+    const key2 = generateKey(doc2);
     expect(key1).to.equal(key2);
   });
 
@@ -93,6 +99,7 @@ describe('Utils Tests', () => {
       title: 'Lorem ipsum dolor sit amet',
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris.',
       tags: ['lorem', 'ipsum', 'dolor', 'sit', 'amet'],
+      signature: null,
     };
 
     const doc2 = {
@@ -103,6 +110,7 @@ describe('Utils Tests', () => {
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris.',
       tags: ['lorem', 'ipsum', 'dolor', 'sit', 'amet'],
       title: 'Lorem ipsum dolor sit amet',
+      signature: null,
     };
 
     expect(doc1).to.deep.equal(doc2);
@@ -110,10 +118,10 @@ describe('Utils Tests', () => {
     // Therefore hashing the string is bad because it's not consistent.
     // The hash of the object must be consistent.
     expect(JSON.stringify(doc1)).to.not.equal(JSON.stringify(doc2));
-    expect(getHash(JSON.stringify(doc1))).to.not.equal(JSON.stringify(doc2));
+    expect(generateKey(JSON.stringify(doc1))).to.not.equal(JSON.stringify(doc2));
 
-    const key1 = getHash(doc1);
-    const key2 = getHash(doc2);
+    const key1 = generateKey(doc1);
+    const key2 = generateKey(doc2);
 
     expect(key1).to.equal(key2);
   });
@@ -122,8 +130,8 @@ describe('Utils Tests', () => {
   const key2 = "1a2b3c4d5e6f7h8"; // Valid: 16 hex characters
   const key3 = "1a2b3c4d5e6f7h8i9"; // Invalid: more than 16 characters
 
-  console.log(isValidKey(key1)); // Output: false
-  console.log(isValidKey(key2)); // Output: true
-  console.log(isValidKey(key3)); // Output: false
+  console.log(isValidKeyForItem(kisValidKeyFormat, ey1)); // Output: false
+  console.log(isValidKeyForItem(kisValidKeyFormat, ey2)); // Output: true
+  console.log(isValidKeyForItem(kisValidKeyFormat, ey3)); // Output: false
   */
 });
