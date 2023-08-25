cbd02767229e15949bb50b9d19f893d2270d83de
tolmdyn <tolmdyn@gmail.com>
2023-08-25 10:55:29 +0100

    moved get/request provider funcs to providers

--- src/network/providers.js
+++ src/network/providers.js
@@ -4,7 +4,12 @@
  * Either on the local cached peers or on the DHT network.
  */
 
+const WebSocket = require('ws');
 const debug = require('debug')('routing');
+
+const { Request, RequestTypes } = require('../models/request');
+const { Response, ResponseTypes } = require('../models/response');
+
 const cache = require('./cache');
 // const Address = require('../models/address');
 
@@ -26,6 +31,109 @@ async function getProviders(key, type) {
   return providers;
 }
 
+/**
+ * @description Generic function to sends a "request" (e.g. GET, PUT, etc) to a
+ * provider.
+ * @param {Request} request The request to send to the providers. Containing the key,
+ * type and/or data of the item.
+ */
+async function sendRequestToProvider(request, provider) {
+  const { ip, port } = provider.lastAddress;
+
+  try {
+    const ws = new WebSocket(`ws://${ip}:${port}`);
+
+    return new Promise((resolve, reject) => {
+      ws.on('error', (err) => {
+        debug('Websocket error:', err);
+        // reject(err);
+        // Should it resolve with the error Response instead?
+        resolve(null);
+      });
+
+      // wait for connection to open before continuing
+      // await new Promise((resolve) => { ws.on('open', resolve); });
+
+      ws.on('open', () => {
+        ws.send(JSON.stringify(request));
+
+        let responseRecieved = false;
+
+        const responsePromise = new Promise((resResolve) => {
+          ws.on('message', (message) => {
+            responseRecieved = true;
+
+            const response = JSON.parse(message);
+            debug('Response:', response);
+
+            if (response.responseType === ResponseTypes.Success) {
+              ws.close();
+              resResolve(response);
+            } else if (response.responseType === ResponseTypes.Error) {
+              ws.close();
+
+              // Should it resolve with the error Response instead?
+              resResolve(null);
+            }
+          });
+        });
+
+        const timeoutPromise = new Promise((_, timeoutReject) => {
+          const timeoutId = setTimeout(() => {
+            if (!responseRecieved) {
+              ws.close();
+              debug('Connection or response timed out.');
+              timeoutReject(new Error('Connection or response timed out.'));
+            }
+          }, 10000); // 10 seconds timeout
+
+          // if response is recieved before timeout, cancel the timeout
+          responsePromise.finally(() => clearTimeout(timeoutId));
+        });
+
+        // Use Promise.race to resolve with the first resolved promise
+        return Promise.race([responsePromise, timeoutPromise])
+          .then((result) => { resolve(result); })
+          .catch((err) => { reject(err); });
+      });
+    });
+  } catch (err) {
+    debug('Error connecting to provider:', err);
+    return null;
+  }
+}
+
+async function sendItemToProviders(item) {
+  const providers = await getProviders(item.key, item.type);
+
+  // if no providers were given, return error
+  if (!providers || providers.length === 0) {
+    return new Response(ResponseTypes.Error, 'No providers found for item.');
+  }
+  debug('Providers:', providers);
+
+  const request = new Request(RequestTypes.Put, { item });
+
+  // map the array of providers to an array of promises
+  const promises = providers.map((provider) => sendRequestToProvider(request, provider));
+
+  // wait for all promises to resolve
+  const results = await Promise.all(promises);
+
+  const successfulResponsesCount = results
+    .filter((result) => result !== null && result.responseType === ResponseTypes.Success).length;
+
+  debug('Number of successful responses:', successfulResponsesCount);
+
+  if (successfulResponsesCount > 0) {
+    return new Response(ResponseTypes.Success, `Item sent to ${successfulResponsesCount} providers.`);
+  }
+
+  // if no successful responses, return error
+  return new Response(ResponseTypes.Error, 'No provider recieved the item.');
+}
 module.exports = {
   getProviders,
+  sendItemToProviders,
+  sendRequestToProvider,
 };
