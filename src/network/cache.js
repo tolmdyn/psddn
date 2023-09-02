b2f64c567e82e5b0a181338a196845f4ef996352
tolmdyn <tolmdyn@gmail.com>
2023-09-02 09:20:49 +0100

    moved handshake peer & ping functions to cache from client

--- src/network/cache.js
+++ src/network/cache.js
@@ -189,6 +189,67 @@ async function requestPeerInfo(address) {
   }
 }
 
+async function handshakePeer(ip, port) {
+  const localPort = localServerPort;
+
+  const ws = new WebSocket(
+    `ws://${ip}:${port}`,
+    { handshakeTimeout: 4000 },
+  );
+
+  return new Promise((resolve, reject) => {
+    ws.on('open', () => {
+      // const request = new Request(RequestTypes.Handshake, { originPeer: getUserSessionKey() });
+
+      const request = new Request(RequestTypes.Handshake, {
+        originKey: getUserSessionKey(), address: { ip, port }, originPort: localPort,
+      });
+
+      ws.send(JSON.stringify(request));
+    });
+
+    ws.on('message', (message) => {
+      const response = JSON.parse(message);
+      ws.close();
+      resolve(response);
+    });
+
+    ws.on('error', (error) => {
+      // console.log('handshakePeer error:', error.message);
+      reject(error);
+    });
+  });
+}
+
+async function pingPeer(ip, port) {
+  try {
+    const ws = new WebSocket(
+      `ws://${ip}:${port}`,
+      { handshakeTimeout: 4000, perMessageDeflate: false },
+    );
+
+    return new Promise((resolve, reject) => {
+      ws.on('open', () => {
+        const request = new Request(RequestTypes.Ping, { targetPeer: null });
+        ws.send(JSON.stringify(request));
+      });
+
+      ws.on('message', (message) => {
+        const response = JSON.parse(message);
+        ws.close();
+        resolve(response);
+      });
+
+      ws.on('error', (error) => {
+        reject(error);
+      });
+    });
+  } catch (error) {
+    debug('pingPeer error:', error);
+    return null;
+  }
+}
+
 /*
 // in server:
 const remoteAddress = request.socket.remoteAddress.replace(/^.*:/, ''); // ipv6 hybrid
@@ -566,4 +627,6 @@ module.exports = {
   // loadCache,
   // saveCache,
   getAllPeers,
+  handshakePeer,
+  pingPeer,
 };
